df65a40736834c79b538c1620165dca997209bd9
Ivo Araujo <ivo.souza-23@outlook.com>
2024-10-08 09:36:27 -0300

    Service working in table-cidades component, On init and Subscribe

--- src/app/table-cidades/table-cidades.component.ts
+++ src/app/table-cidades/table-cidades.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cidades } from '../cidade';
+import { CityService } from '../city.service';
 
 @Component({
   selector: 'app-table-cidades',
   templateUrl: './table-cidades.component.html',
   styleUrl: './table-cidades.component.css'
 })
-export class TableCidadesComponent {
+export class TableCidadesComponent implements OnInit{
+  cidades:Cidades [] = [];
+  constructor(private service:CityService){ }
 
+  ngOnInit(): void {
+    this.loadCities();
+  }
+
+  loadCities(){
+    this.service.getCidades().subscribe({
+      next: data => this.cidades = data
+    });
+  }
 }
