892b4ddf81f197bf69eb9a9c0922f14f081f03b0
Ivo Araujo <ivo.souza-23@outlook.com>
2024-10-10 11:21:16 -0300

    Service ById it's working return me one information throug Id

--- src/app/form-cidades/form-cidades.component.ts
+++ src/app/form-cidades/form-cidades.component.ts
@@ -1,6 +1,7 @@
 import { ActivatedRoute, Router } from '@angular/router';
 import {  } from './../cidade';
 import { Component, OnInit } from '@angular/core';
+import { CityService } from '../city.service';
 
 @Component({
   selector: 'app-form-cidades',
@@ -9,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormCidadesComponent implements OnInit{
   constructor(private router: Router,
-              private activeRouter: ActivatedRoute
+              private activeRouter: ActivatedRoute,
+              private service: CityService
   ){}
 
   ngOnInit() {
-      const id = Number(this.activeRouter.snapshot.paramMap.get("id"));
+    const id = Number(this.activeRouter.snapshot.paramMap.get("id"));
+    this.loadId(id);
   }
+
+  loadId(id:number){
+    this.service.getCitiesById(id).subscribe({
+      next: data => alert(data.city)
+    });
+  }
+
+
 }
