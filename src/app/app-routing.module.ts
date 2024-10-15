5a76f7acfba7b1555c187f36b84e362ab8ebf53f
Ivo Araujo <ivo.souza-23@outlook.com>
2024-10-15 10:32:19 -0300

    CRUD it's Working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormCidadesComponent } from './form-cidades/form-cidades.component';
 import { TableCidadesComponent } from './table-cidades/table-cidades.component';
 
 const routes: Routes = [
-  {path: '', redirectTo:'/cidades', pathMatch:'full'},
+  {path: '', redirectTo:'/home', pathMatch:'full'},
   {path: 'cidades', component: TableCidadesComponent},
   {path: 'cidade/:id', component: FormCidadesComponent},
-  {path: 'cidade', component: FormCidadesComponent}
+  {path: 'cidade', component: FormCidadesComponent},
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,16 @@ import { FormCidadesComponent } from './form-cidades/form-cidades.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TableCidadesComponent,
-    FormCidadesComponent
+    FormCidadesComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
